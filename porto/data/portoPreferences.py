0b8365d3e4ce85efdc82f69c90ff2f3483230738
Pravahati <59502623+Pravahati@users.noreply.github.com>
2023-09-10 16:15:37 +0200

    filename regex

--- porto/data/portoPreferences.py
+++ porto/data/portoPreferences.py
@@ -1,6 +1,31 @@
-"""Data relative to PoRTo's own preferences."""
+"""Contains the data that defines PoRTo's preferences.
 
-meshParentGroup = "mesh"
-riggingModulesParentGroup = "rig"
+Holds variables: filenameElements, fileVersionPadding, meshGroupName,
+riggingModulesGroupName.
 
-# TODO user data folder: where are skinCluster exports saved? next to file?
+Holds regular expressions: filename_characterRegex, filename_stepRegex,
+filename_versionRegexFormat.
+"""
+
+# -------- GLOBALS -------------------------------------------------------------
+filenameElements = ['character', 'step', 'version']
+fileVersionPadding = 3
+meshGroupName = "mesh"
+riggingModulesGroupName = "rig"
+
+
+# -------- REGULAR EXPRESSIONS -------------------------------------------------
+'''Filename regex is built from three regex:
+    - filename_characterRegex: camelCase. Can hold lowercase/uppercase letters,
+    numbers.
+
+    - step: lowercase letters.
+
+    - version: starts with v, V or version; followed by one or several numbers.
+    The amount of numbers is defined by versionPadding.
+'''
+filename_characterRegex = "([a-z](?:[a-z]|[0-9](?![a-z])|[A-Z](?![A-Z])){0,})"
+filename_stepRegex = "([a-z]+)"
+filename_versionRegexFormat = "([vV]|version)([0-9]{versionPadding})"
+
+#
--- porto/portoScene.py
+++ porto/portoScene.py
@@ -13,4 +13,56 @@ def build_main_rigging_group():
     # TODO: GET CHARACTER NAME, GET CHARACTER GROUP, PARENT.
     return
 
+
+def get_filename_format():
+    """Build the expected format for all filenames.
+    
+    The result may change according to what's been specified in PoRTo's
+    preferences.
+    Return a string ready to be formatted, such as '{character}_{step}_{version}
+    """
+    readyToFormatElts = []
+
+    for element in portoPreferences.filenameElements:
+        readyToFormatElt = '{filenameElt}'.replace('filenameElt', element)
+        readyToFormatElts.append(readyToFormatElt)
+
+    return '_'.join(readyToFormatElts)
+
+
+def get_filename_regex():
+    """Return the regular expression that define how a Maya scene file shoud be
+    named."""
+    filenameFormat = get_filename_format()
+
+    formatted = filenameFormat.format(
+        character = portoPreferences.filename_characterRegex,
+        step = portoPreferences.filename_stepRegex,
+        version = get_file_version_regex()
+        )
+    
+    return "^{formatted}$".format(formatted=formatted)
+
+
+def get_file_version_regex():
+    """Return the regex that define how the version of a file should be written."""
+    versionRegexFormat = portoPreferences.filename_versionRegexFormat
+    return versionRegexFormat.replace('versionPadding', str(portoPreferences.fileVersionPadding))
+
+
+def get_file_version():
+    """TODO"""
+    pass
+    return
+
+
+def decompose_filename():
+    """Decompose the filename into a dictionary holding each expected element."""
+    return
+
+
+def increment_save():
+    """Save the file into a new version (increment)."""
+    # TODO
+    return
 #
