241395f9a13985844131a069abf660f3a6ffd9a7
Pravahati <59502623+Pravahati@users.noreply.github.com>
2023-11-09 00:35:07 +0100

    module creation
    nothing to see there yet

---
+++ temp/portoSanityServices.py
@@ -0,0 +1,13 @@
+"""Module holding functions for checking that PoRTo is written as it should"""
+
+
+# TODO - func for checking object naming convention.
+#   classes: PascalCase
+#   functions: snake_case
+#   modules: camelCase
+
+# TODO, func: list all classes in a package/module
+# TODO, func: list all functions in a package/module
+'''inspect module should be good for that'''
+
+#
